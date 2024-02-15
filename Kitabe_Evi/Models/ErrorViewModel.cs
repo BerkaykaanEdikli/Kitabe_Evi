3f3ce1a6dbce1951028ff15712aca0bc1bcd687d
berkay769 <berkay769@BPC03>
2024-02-15 09:15:41 +0300

    Proje dosyası ekle.

---
+++ Kitabe_Evi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Kitabe_Evi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Kitabe_Evi/Models/KitapModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Kitabe_Evi.Models
+{
+    public class KitapModel
+    {
+        public int KitapId { get; set; }
+        public int KitapSayfasayisi { get; set; }
+        public string KitapAdi { get; set; }
+
+    }
+}
